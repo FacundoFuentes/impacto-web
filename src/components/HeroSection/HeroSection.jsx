4c02092942c489b9b1ef095aae52b0456e01bd0c
Facundo Fuentes <facufuentes5335@gmail.com>
2022-02-01 11:58:04 -0300

    HeroSection creada junto a carpetas media

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import estilo from './HeroSection.module.css'
+import logo from '../../media/images/impacto-logo-333-red.png'
+import video from '../../media/videos/Pexels Videos 1716043.mp4'
+
+const HeroSection = () => {
+	return (
+		<div className={estilo.contenedorGeneralNavBar}>
+			<div className={estilo.contenedorMedia}>
+				<img src={logo} alt="Logo Impacto" />
+				<p>THIS IS A SAMPLE TEXT</p>
+				<button>impacto @ gmail.com</button>
+				<video src={video} autoPlay loop muted></video>
+			</div>
+		</div>
+	)
+}
+
+export default HeroSection
